a58010c4abb9ffbeb57ac48426935bd023c54401
yuoo655 <lenuulan@gmail.com>
2023-12-05 14:09:08 +0800

    foo

--- scripts/make/starfive2.mk
+++ scripts/make/starfive2.mk
@@ -1,4 +1,5 @@
 starfive2: build
 	gzip -9 -cvf $(OUT_BIN) > arceos-starfive2.bin.gz
 	mkimage -f tools/starfive2/starfive2_fdt.its arceos_starfive2.itb
-	@echo 'Built the FIT-uImage arceos.itb'
+	cp arceos_starfive2.itb ~/tftproot/
+	@echo 'Built the FIT-uImage arceos_starfive2.itb'
